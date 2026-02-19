c06d1c5b3f2fad60e7f96b0358120d5c7b2921fe
pani79 <emilio.panizza@gmail.com>
2026-02-19 20:54:53 -0300

    26-02-19 ajuste visual

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
+
 export class App {
   protected readonly title = signal('laProfeApp');
 }
