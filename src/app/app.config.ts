72e7a12dcb7d94b20e5ad4b26b47ebee343eee55
pani79 <emilio.panizza@gmail.com>
2026-02-27 01:17:38 -0300

    26-02-27 instalo material y firebase 1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from '../environments/environment';
+
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
